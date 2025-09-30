ed63f61cee5b7c822419ab90d96ea64a10814950
Gustav <gubo0001@stud.kea.dk>
2025-09-30 17:42:21 +0200

    Changes to routes.php deleting not used routes

--- routes.php
+++ routes.php
@@ -11,8 +11,4 @@ post('/login', 'apis/bridge-login.php');
 post('/signup', 'apis/bridge-signup.php');
 get('/logout', 'apis/bridge-logout.php');
 
-// Other APIs
-get('/api-get-user', 'apis/api-get-user.php');
-post('/save-user', 'apis/api-save-user.php');
-
 any('/404','views/404.php');
